175d18713c7215c11bd7a4b2005d86bba1eb7d6e
mgaragiola <marianogaragiola@gmail.com>
2020-01-02 19:18:24 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div id="table-of-contents">
+<h2>Table of Contents</h2>
+<div id="text-table-of-contents">
+<ul>
+<li><a href="#sec-1">1. emacs configuration</a>
+<ul>
+<li><a href="#sec-1-1">1.1. Installation</a></li>
+</ul>
+</li>
+</ul>
+</div>
+</div>
+
+# emacs configuration<a id="sec-1" name="sec-1"></a>
+
+This is my emacs configuration
+
+## Installation<a id="sec-1-1" name="sec-1-1"></a>
+
+Back up you `\~/.emacs.d/` first:
+
+    $ mv ~/.emacs.d ~/emacs_back_up
+
+Clone the repository as the new `\~/.emacs.d/`:
+
+    $ git clone https://github.com/marianogaragiola/emacs_config.git ~/.emacs.d
+
+Open `emacs` and wait until the installation is finished.
---
+++ init.el
@@ -0,0 +1,65 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Setting up the package manager. Install if missing.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
+
+(provide 'init)
+;;; init.el ends here
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "~/.emacs.d/settings.org"
+                   user-emacs-directory))
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(custom-safe-themes
+;;    (quote
+;;     ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "e4a7a2a1d9224751785732a33cc00debfa020ad65d02758bf916f1a8676e7310" default)))
+;;  '(inhibit-startup-screen t)
+;;  '(package-selected-packages
+;;    (quote
+;;     (yasnippet-snippets yasnippet-classic-snippets fill-column-indicator autopair docker-compose-mode markdown-preview-mode dockerfile-mode better-shell toml-mode tabbar multiple-cursors org ##))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+;; (put 'downcase-region 'disabled nil)
+;; (put 'upcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
+ '(package-selected-packages
+   (quote
+    (flx-ido use-package yasnippet-snippets toml-mode tabbar popup neotree multiple-cursors markdown-preview-mode gruvbox-theme flymd flycheck fill-column-indicator elpy dockerfile-mode docker-compose-mode better-shell autopair))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
