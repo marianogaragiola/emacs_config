820150b7829dc506f53ef37ba6ef6ede376e7fd9
marianogaragiola <marianogaragiola@gmail.com>
2020-01-18 17:21:07 -0300

    python mode

---
+++ lisp/init-python.el
@@ -0,0 +1,78 @@
+;; init-python.el --- Initialize python configurations.	-*- lexical-binding: t -*-
+
+;; Copyright (C) 2010-2020 Vincent Zhang
+
+;; Author: Vincent Zhang <seagle0128@gmail.com>
+;; URL: https://github.com/seagle0128/.emacs.d
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+;;; Commentary:
+;;
+;; Python configurations.
+;;
+
+;;; Code:
+
+;; (eval-when-compile
+;;   (require 'init-const)
+;;   (require 'init-custom))
+
+;; Python Mode
+;; Install:
+;;   pip install pyflakes
+;;   pip install autopep8
+(use-package python
+  :ensure nil
+  :hook (inferior-python-mode . (lambda ()
+                                  (process-query-on-exit-flag
+                                   (get-process "Python"))))
+  :init
+  ;; Disable readline based native completion
+  (setq python-shell-completion-native-enable nil)
+  :config
+  ;; Default to Python 3. Prefer the versioned Python binaries since some
+  ;; systems stupidly make the unversioned one point at Python 2.
+  (when (and (executable-find "python3")
+             (string= python-shell-interpreter "python"))
+    (setq python-shell-interpreter "python3"))
+  ;; Env vars
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-env "PYTHONPATH"))
+  ;; Live Coding in Python
+  (use-package live-py-mode)
+
+  ;; Format using YAPF
+  ;; Install: pip install yapf
+  (use-package yapfify
+    :diminish yapf-mode
+    :hook (python-mode . yapf-mode)))
+
+;; (use-package pyenv-mode
+;;   :ensure t)
+(use-package virtualenvwrapper
+  :ensure t
+  :config
+  (setq venv-location "~/Envs/")
+  :hook (venv-workon py3))
+
+(provide 'init-python)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-python.el ends here
