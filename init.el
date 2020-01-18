bf892440fd7091e5e6b6c510722739bbdb84f1c3
marianogaragiola <marianogaragiola@gmail.com>
2020-01-18 10:34:26 -0300

    Several more cool things

--- init.el
+++ init.el
@@ -62,4 +62,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-hl-change ((t (:foreground "#7c6f64"))))
+ '(hl-line ((t (:extend t))))
+ '(pulse-highlight-face ((t (:inherit region))))
+ '(pulse-highlight-start-face ((t (:inherit region))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold))))))
