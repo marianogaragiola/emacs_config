2424b348c56dc060c7e44d96119665a82a8e27e0
marianogaragiola <marianogaragiola@gmail.com>
2020-01-18 21:12:39 -0300

    More and more

--- init.el
+++ init.el
@@ -56,13 +56,14 @@
     ("585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
  '(package-selected-packages
    (quote
-    (flx-ido use-package yasnippet-snippets toml-mode tabbar popup neotree multiple-cursors markdown-preview-mode gruvbox-theme flymd flycheck fill-column-indicator elpy dockerfile-mode docker-compose-mode better-shell autopair))))
+    (virtualenvwrapper pythonic pyenv-mode flycheck-posframe flx-ido use-package yasnippet-snippets toml-mode tabbar popup neotree multiple-cursors markdown-preview-mode gruvbox-theme flymd flycheck fill-column-indicator elpy dockerfile-mode docker-compose-mode better-shell autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(diff-hl-change ((t (:foreground "#7c6f64"))))
+ '(flycheck-posframe-border-face ((t (:inherit default))))
  '(hl-line ((t (:extend t))))
  '(pulse-highlight-face ((t (:inherit region))))
  '(pulse-highlight-start-face ((t (:inherit region))))
